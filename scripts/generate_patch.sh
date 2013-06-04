1cd204bd61b8c4a3f50f0fe602e0ca69925d8527
Volker Braun <vbraun.name@gmail.com>
2013-06-04 11:36:45 +0100

    added script to generate patches

---
+++ scripts/generate_patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+if [ ! -f configure.ac ] ; then
+    echo "You must run this script in the libGAP root directory."
+    exit 1
+fi
+
+VERSION=`autoconf --trace='AC_INIT:$2'`
+
+diff -urd upstream-gap-$VERSION src > patches/gap-$VERSION.patch
+
+
+
+
